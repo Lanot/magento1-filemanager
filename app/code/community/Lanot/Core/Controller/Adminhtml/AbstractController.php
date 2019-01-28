e35029579cc99853b4d39bda48e5c4f42571e672
Anatolii <>
2019-01-28 13:04:49 +0200

    reverted original license

--- app/code/community/Lanot/Core/Controller/Adminhtml/AbstractController.php
+++ app/code/community/Lanot/Core/Controller/Adminhtml/AbstractController.php
@@ -3,7 +3,7 @@
  * Private Entrepreneur Anatolii Lehkyi (aka Lanot)
  *
  * @category    Lanot
- * @package     Lanot_FileManager
+ * @package     Lanot_Core
  * @copyright   Copyright (c) 2010 Anatolii Lehkyi
  * @license     http://opensource.org/licenses/osl-3.0.php
  * @link        http://www.lanot.biz/
